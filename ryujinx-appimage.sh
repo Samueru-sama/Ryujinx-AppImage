7cde3cc6b5d543cd7d2665606835157c5053d77a
Samueru-sama <36420837+Samueru-sama@users.noreply.github.com>
2024-04-11 23:26:44 -0400

    Update ryujinx-appimage.sh

--- ryujinx-appimage.sh
+++ ryujinx-appimage.sh
@@ -55,9 +55,9 @@ chmod a+x ./appimagetool
 ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP.AppDir && 
 ls ./*.AppImage || { echo "appimagetool failed to make the appimage"; exit 1; }
 
-VERSION=$(echo $version | awk -F / '{print $(NF-1)}')
+APPVERSION=$(echo $version | awk -F / '{print $(NF-1)}')
 NAME=$(ls *AppImage)
-mv ./*AppImage ./"$VERSION"-"$NAME"
+mv ./*AppImage ./"$APPVERSION"-"$NAME"
 
 # Clean up
 if [ -z "$APP" ]; then exit 1; fi # Being extra safe lol
