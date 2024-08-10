8d9b19c69750770ce301b875d161618e2d21f6f9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-08-09 23:47:36 -0400

    Update ryujinx-appimage.sh

--- ryujinx-appimage.sh
+++ ryujinx-appimage.sh
@@ -37,7 +37,7 @@ chmod a+x ./appimagetool
 export VERSION="$(echo "$version" | awk -F"/" '{print $(NF-1)}')"
 export ARCH=x86_64
 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
-  -u "gh-releases-zsync|Samueru-sama|Ryujinx-AppImage|continuous|*x86_64.AppImage.zsync" \
+  -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|Ryujinx-AppImage|continuous|*x86_64.AppImage.zsync" \
   ./"$APP".AppDir Ryujinx-"$VERSION"-"$ARCH".AppImage 
 [ -n "$APP" ] && mv ./*.AppImage* .. && cd .. && rm -rf ./"$APP" || exit 1
 echo "All Done!"
